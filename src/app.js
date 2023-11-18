9981b1efb1d7935929fd3b60a1891eed8d106d4f
YakovlevaSS <sofyakovleva95@gmail.com>
2023-11-18 09:53:56 +0300

    corrected path

--- src/app.js
+++ src/app.js
@@ -7,11 +7,14 @@ const server = http.createServer((request, response) =>{
     // - Ответом на запрос `?users` должен быть **JSON** с содержимым файла `data/users.json`, код ответа 200
     // - Если никакие параметры не переданы, то ответ **строка** "Hello, World!", код ответа 200
     // - Если переданы какие-либо другие параметры, то пустой ответ, код ответа 500
-// response.status = 200;
-// response.statusMessage = 
+response.status = 200;
+response.statusMessage = "ok";
+response.header = 'Content-Type: text/plain';
+response.write("Hello, <name>");
+response.end();
 });
 
 server.listen(3003, () => {
-    console.log("Сервер запущен по адресу http://127.0.0.1.3003");
+    console.log("Сервер запущен по адресу http://127.0.0.1:3003");
 })
 
