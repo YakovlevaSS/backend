cfa274d81006a3cd618c806adbdd40df04b9bbf6
Sofia <129661856+YakovlevaSS@users.noreply.github.com>
2023-11-23 23:40:18 +0300

    created corse

--- src/app.js
+++ src/app.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
 const userRouter = require("./routes/users");
 const booksRouter = require("./routes/users");
+
+const corsOption = require('./middleware/cors')
 
 const app = express();
 dotenv.config();
@@ -12,9 +17,13 @@ const { PORT = 3000, API_URL = "http://127.0.0.1" } = process.env;
 //   response.status(200);
 //   response.send("Hello, World!");
 // });
+app.use(cors(corsOption))
+app.use(corsOption);
 
 app.use(userRouter);
 app.use(booksRouter);
+
+app.use(bodyParser.json());
 
 app.listen(PORT, () => {
   console.log(`Ссылка на сервер: ${API_URL}:${PORT}`);
