01968b34fb09dd2ffa7cfa3f13c5f02cfe2acc47
Sofia <129661856+YakovlevaSS@users.noreply.github.com>
2023-11-26 16:57:29 +0300

    corr

--- src/app.js
+++ src/app.js
@@ -15,6 +15,7 @@ const { PORT = 3005, API_URL = "http://127.0.0.1", MONGO_URL = "mongodb://127.0.
 
 app.use(cors())
 app.use(logger);
+app.use(bodyParser.json());
 
 mongoose
   .connect(MONGO_URL)
@@ -24,7 +25,7 @@ mongoose
 app.use(userRouter);
 app.use(booksRouter);
 
-app.use(bodyParser.json());
+
 
 app.listen(PORT, () => {
   console.log(`CORS-enabled, Ссылка на сервер: ${API_URL}:${PORT}`);
--- src/controllers/books.js
+++ src/controllers/books.js
@@ -21,15 +21,13 @@ const getUBookById = (req, res) => {
     });
 };
 
-const createBook = (req, res) => {
-  const data = req.body;
-  books.create(data)
-    .then((book) => {
-      res.status(201).send(book);
-    })
-    .catch((e) => {
-      res.status(500).send(e.message);
-    });
+const createBook = async (req, res) => {
+  try {
+    const book = await books.create({ ...req.body });
+    res.status(201).send(book);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
 };
 
 const updateBook = (req, res) => {
